959b1165676688dc56b55891c56a7e5386898020
deepu901 <134820178+deepu901@users.noreply.github.com>
2023-12-07 19:08:49 +1030

    installed google fonts

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:demo_quiz/answer_button.dart';
 import 'package:demo_quiz/Data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -34,7 +35,11 @@ class _QuestionScreenState extends State<QuestionScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 231, 158, 244),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
